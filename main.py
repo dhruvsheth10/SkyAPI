774387c306be4dd79a3a581982db6d0710dca79e
Dhruv <dhruvsheth10@gmail.com>
2026-05-12 18:13:15 -0700

    add custom 404 page

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 import httpx
 import json
 from contextlib import asynccontextmanager
-
+from fastapi.responses import PlainTextResponse 
 from helper import model_lookup
-from routers import iss, planes
-
-
+from routers import iss, planes, route_bridge
+from fastapi.templating import Jinja2Templates
+from requests import Request
 
 #uvicorn main:app --reload  
 @asynccontextmanager
@@ -18,9 +18,18 @@ async def lifespan(app: FastAPI):
             model_lookup[row['icao']] = row['model']
     yield
     # cleanup here after yield (optional)
-app = FastAPI(title="Welcome to Sky API :)", lifespan=lifespan)
+app = FastAPI(title="Welcome to Sky API :)", lifespan=lifespan,default_response_class=PlainTextResponse)
+
+
+templates = Jinja2Templates(directory="templates")
+
+@app.exception_handler(404)
+async def not_found_exception_handler(request: Request, exc: Exception):
+    return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
+
 @app.get('/')
 def root():
     return "Hello World"
 app.include_router(iss.router)
 app.include_router(planes.router)
+app.include_router(route_bridge.router)
